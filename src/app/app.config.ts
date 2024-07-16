e8101e43d5dfc5a1b16f962cd1ac8a16bbb2b6bd
kumarsachinguri <kumarsachinguri@gmail.com>
2024-07-16 13:09:44 +0530

    Use hashed route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,12 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideExperimentalZonelessChangeDetection(),
   ],
 };

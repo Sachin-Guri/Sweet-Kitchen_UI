e7f8c4d20178c4dd75d08b286d607df88e41a082
kumarsachinguri <kumarsachinguri@gmail.com>
2024-07-16 12:38:00 +0530

    add header + router + footer into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { IconComponent } from './shared/icon/icon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, IconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
